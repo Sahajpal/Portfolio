d3e92d992b57ed82a1ae808c791f7e42fee690ce
Sahajpal <54036040+Sahajpal@users.noreply.github.com>
2022-06-19 10:26:46 +0530

    chore: Integrated with Chakra UI

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,19 +1,19 @@
-const { Box, Container } = require("@chakra-ui/react")
-const { default: Head } = require("next/head")
+const { Box, Container } = require('@chakra-ui/react')
+const { default: Head } = require('next/head')
 
 const Main = ({ children, router }) => {
-    return(
-        <Box as="main" pb={8}>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-                <title>Sahajpal Singh Mahla - Homepage</title>
-            </Head>
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <title>Sahajpal Singh Mahla - Homepage</title>
+      </Head>
 
-            <Container maxW="container.md" pt={14}>
-                {children}
-            </Container>
-        </Box>
-    )
+      <Container maxW="container.md" pt={14}>
+        {children}
+      </Container>
+    </Box>
+  )
 }
 
 export default Main
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-    return (
-        <div>Hello</div>
-    )
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I&apos;m a full-stack developer based in India!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Sahajpal Singh Mahla
+          </Heading>
+          <p>Hardcore Gamer and Crazy Developer ( Web / Android / Smart Contracts ) </p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
