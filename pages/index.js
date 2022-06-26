7bdaeee4d1b2a4ce6faf61d313c71e53eceb3993
Sahajpal <54036040+Sahajpal@users.noreply.github.com>
2022-06-26 20:55:04 +0530

    chore: Added Profile Photo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
-import { Box, Container, Heading } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  Heading,
+  Image,
+  useColorModeValue,
+  chakra
+} from '@chakra-ui/react'
+
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
+})
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I&apos;m a full-stack developer based in India!
       </Box>
 
@@ -12,7 +29,35 @@ const Page = () => {
           <Heading as="h2" variant="page-title">
             Sahajpal Singh Mahla
           </Heading>
-          <p>Hardcore Gamer and Crazy Developer ( Web / Android / Smart Contracts ) </p>
+          <p>
+            Hardcore Gamer and Crazy Developer ( Web / Android / Smart
+            Contracts)
+          </p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/sahaj.jpg"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
         </Box>
       </Box>
     </Container>
