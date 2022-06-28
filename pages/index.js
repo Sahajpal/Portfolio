73bb5a18fa42719d7368b55ad9da43f05a1503ad
Sahajpal <54036040+Sahajpal@users.noreply.github.com>
2022-06-28 16:40:00 +0530

    chore: Added Bio

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
+import NextLink from 'next/link'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 import {
   Box,
   Container,
+  Link,
   Heading,
   Image,
   useColorModeValue,
-  chakra
+  chakra,
+  Button
 } from '@chakra-ui/react'
+import Paragraph from '../components/paragraph'
 import Section from '../components/section'
+import { BioSection, BioYear } from '../components/bio'
 
 const ProfileImage = chakra(Image, {
   shouldForwardProp: prop => ['width', 'height', 'src', 'alt'].includes(prop)
@@ -65,7 +71,62 @@ const Page = () => {
         <Heading as="h3" variant="section-title">
           Work
         </Heading>
-        <p>Paragraph</p>
+        <Paragraph>
+          Self-taught developer with over 3 years of experience in Web and
+          Mobile Development. Performance and efficiency-oriented mindset with a
+          focus on readability and customer/consumer satisfaction. Always keen
+          on learning something new or improving by working on {' '}
+          <Link href="https://github.com/Sahajpal" target="_blank">
+            personal projects 
+          </Link>
+          &nbsp;or reading blogs.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works">
+            <Button
+              rightIcon={<ChevronRightIcon></ChevronRightIcon>}
+              colorScheme="teal"
+            >
+              My Portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>2000</BioYear>
+          Born in Chandigarh (ਚੰਡੀਗੜ੍ਹ), India 🇮🇳.
+        </BioSection>
+        <BioSection>
+          <BioYear>2023</BioYear>
+          Completing my Undergraduate Program in BTech CSE from Bennett
+          &nbsp;University, Greater Noida.
+        </BioSection>
+        <BioSection>
+          <BioYear>2019 - ...</BioYear>
+          Have been practicing and freelancing Full stack Development
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Playing{' '}
+          <Link
+            href="https://steamcommunity.com/profiles/76561198083056940/"
+            target="_blank"
+          >
+            Games
+          </Link>
+          , Drums, Binging Netflix, Read Books, Code some mini projects, hanging
+          out with my friends and Talking to my best friend ISHA🥰 !!!
+        </Paragraph>
       </Section>
     </Container>
   )
