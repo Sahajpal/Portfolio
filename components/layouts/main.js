beead7e61c59073960a1c858ca4e847961a1b801
Sahajpal <54036040+Sahajpal@users.noreply.github.com>
2022-09-20 16:07:07 +0530

    feat: Added a 3D voxel dog with Three.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,18 +1,26 @@
 import Head from 'next/head'
 import Navbar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
+import VoxelDog from '../voxel-dog'
+import NoSsr from '../no-ssr'
 
 const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
         <title>Sahajpal Singh Mahla - Homepage</title>
       </Head>
 
       <Navbar path={router.asPath}></Navbar>
 
       <Container maxW="container.md" pt={14}>
+        <NoSsr>
+          <VoxelDog></VoxelDog>
+        </NoSsr>
         {children}
       </Container>
     </Box>
